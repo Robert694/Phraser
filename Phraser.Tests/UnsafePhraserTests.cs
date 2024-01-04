a3ba5727a525c54f157eba49d985bfd6fb804cbc
Robert <92628251+Robert694@users.noreply.github.com>
2024-01-03 19:27:33 -0500

    Update tests

--- Phraser.Tests/UnsafePhraserTests.cs
+++ Phraser.Tests/UnsafePhraserTests.cs
@@ -1,16 +1,16 @@
-using FluentAssertions;
+﻿using FluentAssertions;
 
 namespace Phraser.Tests
 {
-    public class PhraserTests
+    public class UnsafePhraserTests
     {
         [Theory]
-        [InlineData("hello hello hello world", new[] {"hello hello", "hello", "world"})]
+        [InlineData("hello hello hello world", new[] { "hello hello", "hello", "world" })]
         [InlineData("hello hello test hello world", new[] { "hello hello", null, "hello", "world" })]
         [InlineData("hello world", new[] { "hello", "world" })]
         public void Parse_ShouldBeExpectedResult(string input, string[] expectedResult)
         {
-            IPhraseParser<string> parser = new PhraseParser<string>();
+            IPhraseParser<string> parser = new UnsafePhraseParser<string>();
             IPhraseSupplier<string> loader = new MemoryPhraseSupplier<string>(
         [
             new(["hello"], "hello"),
@@ -31,7 +31,7 @@ namespace Phraser.Tests
         [InlineData("hello world", new[] { "hello world" })]
         public void Parse_ShouldNotBeExpectedResult(string input, string[] expectedResult)
         {
-            IPhraseParser<string> parser = new PhraseParser<string>();
+            IPhraseParser<string> parser = new UnsafePhraseParser<string>();
             IPhraseSupplier<string> loader = new MemoryPhraseSupplier<string>(
         [
             new(["hello"], "hello"),
@@ -44,16 +44,6 @@ namespace Phraser.Tests
                 .Select(x => x.Value)
                 .Should()
                 .NotEqual(expectedResult);
-        }
-
-        [Theory]
-        [InlineData("hello hello hello world", new[] { "hello", "hello", "hello", "world" })]
-        [InlineData("hello hello      .....       hello world!!!", new[] { "hello", "hello", "hello", "world" })]
-        public void Sanitize_ShouldBeExpectedResult(string input, string[] expectedResult)
-        {
-            PhraseParser.Sanitize(input.Split())
-                .Should()
-                .Equal(expectedResult);
         }
     }
 }
